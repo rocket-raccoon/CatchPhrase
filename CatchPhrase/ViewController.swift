a4eee122d14195de5c9906333a593759c9f61468
Alberto Benavides <alberto_benavides@Albertos-MacBook-Pro.local>
2014-12-26 11:47:26 -0800

    First commit

---
+++ CatchPhrase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatchPhrase
+//
+//  Created by Alberto Benavides on 12/26/14.
+//  Copyright (c) 2014 rocket-raccoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
