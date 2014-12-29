04161f5d559ad56c8009ecebc3ccbbddeaf4b7a1
rocket-raccoon <the.alberto.bena@gmail.com>
2014-12-28 17:53:11 -0800

    Added segue to rebuttal page

--- CatchPhrase/EndRoundViewController.swift
+++ CatchPhrase/EndRoundViewController.swift
@@ -15,24 +15,20 @@ class EndRoundViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
-
+    
+    @IBAction func nameTeam2Winner(sender: UIButton) {
+        scoreKeeper.incrementTeam1Score()
+        performSegueWithIdentifier("goToRebuttal", sender: nil)
+    }
+    
+    @IBAction func nameTeam1Winner(sender: UIButton) {
+        scoreKeeper.incrementTeam2Score()
+        performSegueWithIdentifier("goToRebuttal", sender: nil)
+    }
+    
 }
---
+++ CatchPhrase/RebuttalViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RebuttalViewController.swift
+//  CatchPhrase
+//
+//  Created by Alberto Benavides on 12/28/14.
+//  Copyright (c) 2014 rocket-raccoon. All rights reserved.
+//
+
+import UIKit
+
+class RebuttalViewController: UIViewController {
+    
+    var scoreKeeper:ScoreKeeper!
+    var phraseBank:PhraseBank!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
