3f34f32c269c75e67166c88113fd173a3e8c1e42
rocket-raccoon <the.alberto.bena@gmail.com>
2015-01-15 18:42:15 -0500

    Coded screen for countdown timer round

--- CatchPhrase/GuessRoundViewController.swift
+++ CatchPhrase/GuessRoundViewController.swift
@@ -15,17 +15,26 @@ class GuessRoundViewController: UIViewController, AVAudioPlayerDelegate {
     var scoreKeeper:ScoreKeeper!
     var audioPlayer:AVAudioPlayer!
     
-    @IBOutlet weak var phraseTextView: UILabel!
-    @IBOutlet weak var pauseButton: UIButton!
-    @IBOutlet weak var bottomView: UIView!
-    @IBOutlet weak var topView: UIView!
+    var headerLabel: UILabel!
+    var phraseLabel: UILabel!
+    var pauseButton: UIButton!
+    var skipButton: UIButton!
+    var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Set up the views on the screen
+        setupHeaderLabel()
+        setupPhraseLabel()
+        setupPauseButton()
+        setupSkipButton()
+        setupNextButton()
+        horizontallyCenterViews([skipButton, nextButton, pauseButton, headerLabel])
+        verticallyOrientViews()
         //Put up the phrase on screen
         let phrase = phraseBank.getNextPhrase()
-        phraseTextView.text = phrase
-        phraseTextView.font = UIFont(name: phraseTextView.font.fontName, size: 20)
+        phraseLabel.text = phrase
+        phraseLabel.font = UIFont(name: phraseLabel.font.fontName, size: 20)
         //Set up the audio for the countdown timer
         let filePath = NSBundle.mainBundle().pathForResource("countdown", ofType: ".mp3")
         let filePathUrl = NSURL(fileURLWithPath: filePath!)
@@ -39,8 +48,89 @@ class GuessRoundViewController: UIViewController, AVAudioPlayerDelegate {
         self.navigationItem.rightBarButtonItem = b
     }
     
-    func exitGame() {
-        performSegueWithIdentifier("exitGame", sender: nil)
+    func setupHeaderLabel() {
+        //Create the header label
+        var headerLabelFrame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        headerLabel = UILabel(frame: headerLabelFrame)
+        headerLabel.text = "Your phrase is:"
+        headerLabel.sizeToFit()
+        headerLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addSubview(headerLabel)
+    }
+    
+    func setupPhraseLabel() {
+        //Create the phrase label
+        var phraseLabelFrame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 500)
+        phraseLabel = UILabel(frame: phraseLabelFrame)
+        phraseLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        phraseLabel.textAlignment = NSTextAlignment.Center
+        view.addSubview(phraseLabel)
+        //Set width equal to screen width
+        let widthConst = NSLayoutConstraint(item: phraseLabel, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1.0, constant: 0.0)
+        view.addConstraint(widthConst)
+    }
+    
+    func setupPauseButton() {
+        //Create the pause button
+        var pauseButtonFrame = CGRect(x: 0, y: 0, width: 0, height: 60)
+        pauseButton = UIButton(frame: pauseButtonFrame)
+        pauseButton.backgroundColor = UIColor.blueColor()
+        pauseButton.layer.cornerRadius = 10
+        pauseButton.setTitle("Pause", forState: .Normal)
+        pauseButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        pauseButton.addTarget(self, action: Selector("pauseGame"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(pauseButton)
+        //Set width to be proportional to screen width
+        let widthConst = NSLayoutConstraint(item: pauseButton, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.8, constant: 0.0)
+        view.addConstraint(widthConst)
+    }
+    
+    func setupSkipButton() {
+        //Create the skip button
+        var skipButtonFrame = CGRect(x: 0, y: 0, width: 0, height: 60)
+        skipButton = UIButton(frame: skipButtonFrame)
+        skipButton.backgroundColor = UIColor.blueColor()
+        skipButton.layer.cornerRadius = 10
+        skipButton.setTitle("Skip", forState: .Normal)
+        skipButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        skipButton.addTarget(self, action: Selector("skipPhrase"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(skipButton)
+        //Set width to be proportional to screen width
+        let widthConst = NSLayoutConstraint(item: skipButton, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.8, constant: 0.0)
+        view.addConstraint(widthConst)
+    }
+    
+    func setupNextButton() {
+        //Create the next button
+        var nextButtonFrame = CGRect(x: 0, y: 0, width: 0, height: 60)
+        nextButton = UIButton(frame: nextButtonFrame)
+        nextButton.backgroundColor = UIColor.blueColor()
+        nextButton.layer.cornerRadius = 10
+        nextButton.setTitle("Next", forState: .Normal)
+        nextButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        nextButton.addTarget(self, action: Selector("nextPhrase"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(nextButton)
+        //Set width to be proportional to screen width
+        let widthConst = NSLayoutConstraint(item: nextButton, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.8, constant: 0.0)
+        view.addConstraint(widthConst)
+    }
+    
+    func horizontallyCenterViews(viewsToCenter: [UIView]) {
+        for v in viewsToCenter {
+            let horizontalCenterConst = NSLayoutConstraint(item: v, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            view.addConstraint(horizontalCenterConst)
+        }
+    }
+    
+    func verticallyOrientViews() {
+        //Vertically align header and phrase labels just below top of screen
+        var labelsDictionary = ["headerLabel": headerLabel, "phraseLabel": phraseLabel]
+        var labelsVerticalConst = NSLayoutConstraint.constraintsWithVisualFormat("V:|-75-[headerLabel]-[phraseLabel]", options: nil, metrics: nil, views: labelsDictionary)
+        view.addConstraints(labelsVerticalConst)
+        //Vertically align buttons just above bottom of screen
+        var buttonsDictionary = ["pauseButton": pauseButton, "skipButton": skipButton, "nextButton": nextButton]
+        var buttonsVerticalConst = NSLayoutConstraint.constraintsWithVisualFormat("V:[pauseButton]-[skipButton]-[nextButton]-30-|", options: nil, metrics: nil, views: buttonsDictionary)
+        view.addConstraints(buttonsVerticalConst)
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,47 +138,40 @@ class GuessRoundViewController: UIViewController, AVAudioPlayerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func pauseCountdown(sender: UIButton) {
+    func pauseGame() {
         let state = pauseButton.titleLabel!.text
         if (state == "Resume") {
             audioPlayer.stop()
             audioPlayer.play()
             pauseButton.setTitle("Pause", forState: UIControlState.Normal)
-            pauseButton.backgroundColor = UIColor.redColor();
         } else {
             audioPlayer.stop()
             pauseButton.setTitle("Resume", forState: UIControlState.Normal)
-            pauseButton.backgroundColor = UIColor.greenColor()
         }
     }
     
     func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
         if flag {
-            performSegueWithIdentifier("endRound", sender: nil)
-        }
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if(segue.identifier == "endRound") {
-            var endRoundVC = segue.destinationViewController as EndRoundViewController
+            var endRoundVC = EndRoundViewController()
             endRoundVC.phraseBank = phraseBank
             endRoundVC.scoreKeeper = scoreKeeper
+            navigationController?.pushViewController(endRoundVC, animated: false)
         }
     }
     
-    @IBAction func skipPhrase(sender: UIButton) {
+    func skipPhrase() {
         if scoreKeeper.skipsLeft() {
             let phrase = phraseBank.getNextPhrase()
-            phraseTextView.text = phrase
+            phraseLabel.text = phrase
             scoreKeeper.curSkips += 1
         } else {
             println("No more skips for this round")
         }
     }
     
-    @IBAction func getNextPhrase(sender: UIButton) {
+    func nextPhrase() {
         let phrase = phraseBank.getNextPhrase()
-        phraseTextView.text = phrase
+        phraseLabel.text = phrase
         scoreKeeper.curSkips = 0
     }
     
